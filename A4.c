5d9c645171ab5600bd221a0f1b1ae546ba1d3850
kn33hit <npatel10@umbc.edu>
2014-10-28 22:43:59 -0400

    Update A4.c

--- A4.c
+++ A4.c
@@ -1,21 +1,40 @@
+/*                                                                              
+  AUTHORS:                                                                      
+     Neh Patel                                                                  
+     Patrick Custer                                                             
+     Tommy                                                                      
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+/*========================================================================*/
 /* DECLARATION OF FUNCTIONS */
 int doDifferentEquations(int LHS[], int RHS[], int size);
 int checkOps(int LHS[], int RHS[]);
+/*=========================================================================*/
+
 
 /* FUNCTIONS */
-int doDifferentEquations(int LHS[], int RHS[], int LHSsize){
-  /* recursive, calls itself till checkOps returns a 1 */
+/* The whole concept is to have the original list chooped into two different    
+   lists called Right-hand side and Left-hand side. And call the checkOPs       
+   function and pass these two lists into it, sum up both lists                 
+   with different combinations. Then if two side end up being equal             
+   the equations is printed out and both functions return positive values       
+   meaning that an actual equation would exist out of the numbers entered */
+/*=========================================================================*/
+int doDifferentEquations(int LHS[], int RHS[], int LHSsize)
+{
   /* FIRST BASE CASE. */
   if(LHSsize == 1){
     /* when the size of lhs is just one, we still want to go through it         
-       but the last thing so return it */
+       but the last thing so return after that */
     return 0;
   }
-  /* SECOND BASE CASE */
+ /* SECOND BASE CASE */
   int z = checkOps(LHS,RHS);
+  /* if z is 1, means we've found out equation and now we need to get out of    
+     this recursive loop */
   if(z == 1){
     return 1;
   }else{
@@ -25,13 +44,12 @@ int doDifferentEquations(int LHS[], int RHS[], int LHSsize){
     /* one size big */
     int newRHS[LHSsize+1];
     /* making the new LHS, size - 1 to have 1 less size */
-    int a=0,b=0;
-    while( a < LHSsize -1){
+    int a = 0,b = 0;
+    while( a < LHSsize - 1){
         newLHS[a] = LHS[a];
         a++;
     }
-
-    /* only regular size to copy the whole thing */
+      /* only regular size to copy the whole thing */
     while(b < LHSsize){
       newRHS[b] = RHS[b];
       b++;
@@ -43,10 +61,28 @@ int doDifferentEquations(int LHS[], int RHS[], int LHSsize){
     LHSsize--;
     /* RECURSION */
     int y = doDifferentEquations(newLHS,newRHS,LHSsize);
+    /* same goes for the y here. It just exists because this function           
+       returns an int which is used in the main as the boolean.                 
+       So the next line is pointless, but is there to avoid the                 
+       warning prompt */
+    y = 0;
   }
+   /* we dont really need another return, but since there is a warning           
+     error, we will just return a 0 here to avoid it.                           
+     There will be no effect of this line in this function but to avoid         
+     a warning */
+  return 0;
 }
 
-int checkOps(int LHS[], int RHS[]){
+/*=========================================================================*/
+/*  CHECK OPS  woudld now have the list of left side and right side.            
+    Now it will make random combinations of equations for both sides            
+    and then compute it. After doing that for both sides, it will               
+    compare the sums, and if they match it will return 1, if                    
+    not then it may return any other number.                                    
+*/
+int checkOps(int LHS[], int RHS[])
+{
   /* printing the equation happens here, and returning 1 or o */
   if(LHS == RHS){
     printf("They are equal!!\n");
@@ -55,11 +91,15 @@ int checkOps(int LHS[], int RHS[]){
   return 2;
 }
 
+
+/*=========================================================================*/
 /* MAIN */
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
   /* the length of the list is argc -1 one since we need get rid of the         
      initial filename entered 2*/
-  int LHS[argc-1];
+
+int LHS[argc-1];
   int RHS[argc-1];
   if(argc > 1){
     int x=1;
@@ -69,18 +109,51 @@ int main(int argc, char *argv[]){
       a = strtol(argv[x], NULL, 0);
       /* have to use x-1 since arrays are assigned form 0, we staretd  from     
          1 in the loop */
+      /* only initializing this list, Rhs is created later                      
+         as we need to split the list first */
       LHS[x-1] = a;
-      RHS[x-1]=a;
       x++;
     }
   }else{
     printf("Enter some arguments to run the program\n");
   }
-  /*                                                                            
-    FOR NOW IN COMMENTS, MAYBE PRINT THIS MESSAGE IN THE FUNC */
+
+  int t = 0;
+  int size = argc - 1;
+  if((size % 2) == 0){
+    /* if even number of numbers are given to us */
+    /* loop through until half the size is done */
+    while(t < ((argc-1) / 2)){
+        LHS[size-1] = RHS[t];
+        LHS[size-1] = 0;
+        size--;
+        t++;
+    }
+  }else{
+    /* if odd number of numbers are given to us */
+    while(t < ((argc-1) / 2)){
+      /* take the last number in the LHS and push it                            
+         into the RHS */
+      LHS[size-1] = RHS[t];
+      LHS[size-1] = 0;
+      size--;
+      t++;
+    }
+  /* trying to have one less number in the left hand side                     
+       so replace one more time */
+    LHS[size-1] = RHS[t];
+    LHS[size-1] = 0;
+    /* this is broken because zero is still accounted in here                   
+       and we do not want that */
+  }
+
   int z = doDifferentEquations(LHS,RHS,argc-1);
+  /* the integer returned from the doDifferentEquations function                
+     is used in a conditional like a boolean to print the message               
+     that we were unable to come up with an equation.                           
+     Which will be the case here everytime */
   if(z != 1){
-    printf("This equaiton is not possible.\n");
+    printf("No equation found.\n");
     }
   return 0;
-  }
+}
